58fbb620331cc2582da93db7a14b54d197777c4d
Edmund Home <edmundhome@hotmail.com>
2020-06-02 23:23:21 +0200

    Nino
    Validación

--- Proyecto2/Proyecto2/src/main/java/com/example/proyecto2/model/Perfil.java
+++ Proyecto2/Proyecto2/src/main/java/com/example/proyecto2/model/Perfil.java
@@ -22,6 +22,7 @@ public class Perfil implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	@Id
+	//Nino Revisar mañana
 	@Size(min=3, max=40) 
 	private String nickName;
 	
