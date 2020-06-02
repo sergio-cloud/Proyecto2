6d3286ce8ffd5eb15c7ea5e8021c7f24832b7e60
sergio-cloud <1996sml@gmail.com>
2020-06-02 19:35:07 +0200

    CONFIG Y PERFIL CAMBIOS

---
+++ Proyecto2/Proyecto2/src/main/java/com/example/proyecto2/config/ConfigForm.java
@@ -0,0 +1,19 @@
+package com.example.proyecto2.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ConfigForm implements WebMvcConfigurer  {
+	
+	@Bean
+	public MessageSource messageSource() {
+	    ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+	    messageSource.setBasename("messages");
+	    return messageSource;
+	}
+
+}
