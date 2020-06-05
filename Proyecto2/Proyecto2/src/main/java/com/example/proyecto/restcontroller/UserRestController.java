221fd528a94ccb642471a7269928ff228fa3927e
Edmund Home <edmundhome@hotmail.com>
2020-06-05 18:09:03 +0200

    Nino
    Recuperacion de clases

---
+++ Proyecto2/Proyecto2/src/main/java/com/example/proyecto/restcontroller/UserRestController.java
@@ -0,0 +1,57 @@
+package com.example.proyecto.restcontroller;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.proyecto2.model.Perfil;
+import com.example.proyecto2.model.Poblacion;
+import com.example.proyecto2.service.PerfilService;
+import com.example.proyecto2.service.PoblacionService;
+@RestController
+public class UserRestController {
+	@Autowired
+	PoblacionService PoblacionService;
+	
+	@Autowired
+	PerfilService PerfilService;
+	
+	
+	//LISTADO DE TODAS LAS POBLACIONES   NINO
+	@RequestMapping(
+			value = "/poblacion/listado",
+			method = RequestMethod.GET,
+			headers = {"Accept=application/json" },
+			produces = "application/json; charset=utf-8")
+	
+	public List<Poblacion> getPoblacion() {
+		return PoblacionService.findAll();
+	}
+	
+	//DEVUELVE UNA POBLACION (idPoblacion)    SERGIO
+	@RequestMapping(
+			value = "/poblacion/{idPoblacion}",
+			method = RequestMethod.GET,
+			headers = {"Accept=application/json" },
+			produces = "application/json; charset=utf-8")
+	
+	public Optional<Poblacion> getPoblacionByIdPoblacion(@PathVariable int idPoblacion) {
+		return PoblacionService.findByPoblacion(idPoblacion);
+	}
+	
+	
+	
+	//DEVUELVE LISTADO DE TODOS LOS PERFILES REGISTRADOS   NINO
+	@RequestMapping(
+			value = "/perfil/listado",
+			method = RequestMethod.GET,
+			headers = {"Accept=application/json" },
+			produces = "application/json; charset=utf-8")
+	
+	public List<Perfil> getPerfiles() {
+		return PerfilService.findAll();
+	}
+	
+}
---
+++ Proyecto2/Proyecto2/src/main/java/com/example/proyecto2/util/GeneradorPerfiles.java
@@ -0,0 +1,40 @@
+package com.example.proyecto2.util;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Autowired;
+import com.example.proyecto2.model.Perfil;
+import com.example.proyecto2.model.Poblacion;
+import com.example.proyecto2.service.PerfilService;
+//import com.example.proyecto2.service.PoblacionService;
+import com.github.javafaker.Faker;
+public class GeneradorPerfiles {
+	EntityManager em;
+	
+	public static Perfil Generar() {
+		Poblacion poblacion = new Poblacion();
+		Faker faker = new Faker();
+		Perfil aux = new Perfil();
+		aux.setNickName(faker.name().fullName() + faker.number().numberBetween(15, 99));
+		aux.setEdad(faker.number().numberBetween(18, 60));
+		if (faker.number().numberBetween(0, 1) == 0) {
+			aux.setGenero("Hombre");
+		} else
+			aux.setGenero("Mujer");
+		aux.setPassword("1234");
+		poblacion.setIdPoblacion(faker.number().numberBetween(4, 54));
+		poblacion.setNombrePoblacion("Madrid");
+		aux.setPoblacion(poblacion);
+		System.out.println("\n"+aux.toString());
+		return aux;
+	}
+/*
+	public static List<Perfil> generarLista(int cantidad) {
+		List<Perfil> listPerfil = new ArrayList<Perfil>();
+		for (int i = 0; i < cantidad; i++) {
+			listPerfil.add(Generar());
+		}
+		return listPerfil;
+	}*/
+}
