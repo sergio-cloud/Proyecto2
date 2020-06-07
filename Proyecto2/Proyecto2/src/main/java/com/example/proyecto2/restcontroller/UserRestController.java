10265ea1fb833863a1590fa81f0cdca331b4d48c
Edmund Home <edmundhome@hotmail.com>
2020-06-07 17:36:38 +0200

    Nino
    Logger

--- Proyecto2/Proyecto2/src/main/java/com/example/proyecto2/restcontroller/UserRestController.java
+++ Proyecto2/Proyecto2/src/main/java/com/example/proyecto2/restcontroller/UserRestController.java
@@ -1,11 +1,16 @@
 package com.example.proyecto2.restcontroller;
 import java.util.List;
 import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+
 import com.example.proyecto2.model.Perfil;
 import com.example.proyecto2.model.Poblacion;
 import com.example.proyecto2.service.PerfilService;
@@ -18,6 +23,7 @@ public class UserRestController {
 	@Autowired
 	PerfilService PerfilService;
 	
+	private static final Logger logger = LoggerFactory.getLogger(UserRestController.class);
 	
 	//LISTADO DE TODAS LAS POBLACIONES   NINO
 	@RequestMapping(
@@ -27,6 +33,7 @@ public class UserRestController {
 			produces = "application/json; charset=utf-8")
 	
 	public List<Poblacion> getPoblacion() {
+		logger.info("---En listado: Poblaciones");
 		return PoblacionService.findAll();
 	}
 	
@@ -38,10 +45,11 @@ public class UserRestController {
 			produces = "application/json; charset=utf-8")
 	
 	public Optional<Poblacion> getPoblacionByIdPoblacion(@PathVariable int idPoblacion) {
+		logger.info("---Obteniendo población por Id---");
 		return PoblacionService.findByPoblacion(idPoblacion);
 	}
 	
-	
+
 	
 	//DEVUELVE LISTADO DE TODOS LOS PERFILES REGISTRADOS   NINO
 	@RequestMapping(
@@ -51,6 +59,7 @@ public class UserRestController {
 			produces = "application/json; charset=utf-8")
 	
 	public List<Perfil> getPerfiles() {
+		logger.info("---En listado: Perfil");
 		return PerfilService.findAll();
 	}
 	
